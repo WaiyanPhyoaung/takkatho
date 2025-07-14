1f378a33e18883932df36f931178d0016589467c
WaiyanPhyoaung <waiyanphyoaung2015@gmail.com>
2025-07-14 23:19:48 +0630

    feat: C4 architecture page

--- astro.config.mjs
+++ astro.config.mjs
@@ -594,6 +594,10 @@ export default defineConfig({
                   label: "Trade-Off & Making Decisions",
                   slug: "software-architecture/craft/decisions",
                 },
+                {
+                  label: "C4 Model",
+                  slug: "software-architecture/craft/c4",
+                },
               ],
             },
           ],
