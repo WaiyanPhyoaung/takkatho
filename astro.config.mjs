47636aa01a0cdf6640de67862d0213c056632200
pmyatthwe <phumyatthwe19@gmail.com>
2025-07-23 18:50:50 +0800

    feat: add sql querying, filtering and sorting

--- astro.config.mjs
+++ astro.config.mjs
@@ -738,36 +738,30 @@ export default defineConfig({
                 {
                   label: "Fifth Normal Form",
                   slug: "relational-database/normalization/fifth-normal-form",
-<<<<<<< HEAD
                 },
               ],
-            },
-=======
-                }
-              ]
             },
             {
               label: "SQL",
               items: [
                 {
                   label: "SQL",
-                  slug: "relational-database/sql/what-is-sql"
+                  slug: "relational-database/sql/what-is-sql",
                 },
                 {
                   label: "SQL Basic Types",
-                  slug: "relational-database/sql/sql-types"
+                  slug: "relational-database/sql/sql-types",
                 },
                 {
                   label: "Querying Data",
-                  slug: "relational-database/sql/querying-data"
+                  slug: "relational-database/sql/querying-data",
                 },
                 {
                   label: "Filtering and Sorting Data",
-                  slug: "relational-database/sql/filter-and-sort-data"
-                }
-              ]
-            }
->>>>>>> c58b60b (feat: add sql querying, filtering and sorting)
+                  slug: "relational-database/sql/filter-and-sort-data",
+                },
+              ],
+            },
           ],
         },
         // {
